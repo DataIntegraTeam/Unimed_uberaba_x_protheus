59d9b26de0419b82274c19eb09b6eb3863c79a22
DATA INTEGRA <marcelo.a.marcal@hotmail.com>
2022-06-21 09:02:45 -0300

    Alteração de campos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import { unitsRoutes } from './unitsRoutes';
-import { professionalsRoutes } from './professionalsRoutes';
 import { slotsRoutes } from './slotsRoutes';
 import { movementsRoutes } from './movementsRoutes';
-import { cancelingRoutes } from './cancelingRoutes';
-import { revenuesRoutes } from './revenuesRoutes';
+import { inventorysRoutes } from './inventorysRoutes';
 
 import { authentication } from '../middleware/authentication';
 
@@ -14,10 +12,8 @@ const router = Router();
 router.use(authentication);
 
 router.use('/api/v1', unitsRoutes);
-router.use('/api/v1', professionalsRoutes);
 router.use('/api/v1', slotsRoutes);
-router.use('/api/v1', cancelingRoutes);
+router.use('/api/v1', inventorysRoutes);
 router.use('/api/v1', movementsRoutes);
-router.use('/api/v1', revenuesRoutes);
 
 export { router };
