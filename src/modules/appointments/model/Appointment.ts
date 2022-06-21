6629bde0a56a8123a73feee280bd34999a187d6e
Jorge Barcellos <jorgedataintegra@gmail.com>
2022-06-21 09:12:47 -0300

    Alteração campos

--- src/modules/appointments/model/Appointment.ts
+++ src/modules/appointments/model/Appointment.ts
@@ -1,40 +1,42 @@
 import { v4 as uuidV4 } from 'uuid';
 
 class Appointment {
-  id: string;
-  cd_dti_agenda?: number;
-  tp_fluxo: string;
-  tp_status: string;
-  ds_erro: string;
-  dt_gerado: string;
-  tp_registro: string;
-  dt_processado: string;
-  tp_movimento: string;
-  cd_multi_empresa: string;
-  reason: string;
-  cd_agendamento_integra: string;
-  appointmentId: string;
-  slotId: string;
-  professionalId: string;
-  unitId: string;
-  productId: string;
-  telemedicine: string;
-  patient: {
-    benefitCode: string;
-    phone: string;
-    email: string;
-    name: string;
-    birthDate: Date;
-    document: {
-      type: string;
-      number: string;
-    };
-  };
+  Document: {
+    nrFilial: number; // "filial":"001001"
+    dcNumero: number; //"numero":"123456789"
+    tpObservacao: string; //"texto obeservacao"
+    nrCustos: number; //"ccustos":"252627"
+    nrCcontail: number; //"ccontail":"353637"
+  }
+
+  Auditoria: {
+    nmUsuario: string; //"admin"
+    nrData: Date; //data":"15/01/2022" e "hora":"15:30:25"
+    
+ }
+
+  Itens: {
+    nrProduto: number; //"produto":"00100100"
+    lote: string; //"lote":"ABCDEF",
+    dtValidade: Date; //"validade":"31/12/2023",
+    tpUnidade: string; //"unidade":"CX",
+    cdArmazem: number; //"armazem":"10",
+    qtItem: number; //"contagem":13
+  /* VERIFICAR <====
+    {
+      "produto":"00100200",
+      "lote":"GJHTUV",
+      "validade":"31/12/2023",
+      "unidade":"ML",
+      "armazem":"10",
+      "contagem":25
+    }*/
+  }
+
   constructor() {
-    if (!this.id) {
-      this.id = uuidV4();
+    
     }
   }
-}
+
 
 export { Appointment };
